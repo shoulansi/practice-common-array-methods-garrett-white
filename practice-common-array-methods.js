cfd522a8dba573d5c4e7fff54ac23b1d2ad57104
shoulansi <garrettswhite@gmail.com>
2025-03-21 13:04:11 -0500

    final practice

--- practice-common-array-methods.js
+++ practice-common-array-methods.js
@@ -1,1 +1,27 @@
+let orders = [
+    ["Coffee", "Tea", "Orange Juice"],
+    ["Donut", "Bagel", "Muffin"]
+];
 
+console.log(orders[0].length);
+console.log(orders[1].length);
+
+console.log(orders[0][1]);
+console.log(orders[1][2]);
+
+console.log(orders[0][0]);
+console.log(orders[1][1]);
+
+console.log(orders[0][2]);
+console.log(orders[1][0]);
+
+let row = 1
+let item = 2
+console.log(orders[row][item]);
+
+for(let i = 0; i < orders[0].length; i++) {
+    console.log(orders[0][i]);
+}
+
+orders[0].push("Flat White");
+console.log(orders[0].length);
